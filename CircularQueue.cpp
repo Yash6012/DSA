1d2da241631f4c65543098b6b8f40557a3f15033
yash6012 <yash7singh3@gmail.com>
2025-06-20 22:10:49 +0530

    circular commit

---
+++ CircularQueue.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+class CircularQueue
+{
+private:
+    int* arr;
+    int n;
+    int front;
+    int rear;
+public:
+    CircularQueue(int size) {
+        this -> n = size;
+        arr = new int[size];
+        front = -1;
+    }
+    void push(int val) {
+        // 4 cases
+        // 1. Overflow
+        if ((front == 0 && rear == n-1) || (rear = front - 1)) {
+            cout << "Overflow" << endl;
+        }
+        // 2. First Element
+        else if (front == -1 && rear == -1) {
+            front++;
+            rear++;
+            arr[rear] = val;
+        }
+        // 3. Circular nature
+        else if (rear == n - 1 && front != 0) {
+            rear = 0;
+            arr[rear] = val;
+        }
+        // 4. Normal
+        else {
+            rear++;
+            arr[rear] = val;
+        }
+    }
+    void pop() {
+        // 4. Case
+        // 1. Underflow
+        if (front == -1 && rear == -1) {
+            cout << "UnderFlow" << endl;
+        }
+        // 2. single element
+        else if (front == rear) {
+            arr[front] = -1;
+            front = -1;
+            rear = -1;
+        }
+        // 3. circular case
+        else if (front == n - 1) {
+            arr[front] = -1;
+            front = 0;
+        }
+        else {
+            arr[front] = -1;
+            front++;
+        }
+    }
+};
+
+CircularQueue::CircularQueue(int size)
+{
+}
+
+CircularQueue::~CircularQueue()
+{
+}
