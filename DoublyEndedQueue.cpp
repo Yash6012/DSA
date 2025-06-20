e231db434721496ae8b6e996e7eaaa0ec4f0ddd7
yash6012 <yash7singh3@gmail.com>
2025-06-20 21:12:32 +0530

    test commit

---
+++ DoublyEndedQueue.cpp
@@ -0,0 +1,29 @@
+class DoublyEndedQueue
+{
+private:
+    int* arr;
+    int n;
+    int front;
+    int rear;
+public:
+    DoublyEndedQueue(int size) {
+        this -> n = size;
+        arr = new int[size];
+        this -> front = -1;
+        this -> rear = -1; 
+    }
+    void pushFront(int val) {
+        if (front )
+    }
+    void pushBack(int val) {
+        
+    }
+};
+
+DoublyEndedQueue::DoublyEndedQueue(int size)
+{
+}
+
+DoublyEndedQueue::~DoublyEndedQueue()
+{
+}
