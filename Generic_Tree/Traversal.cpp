79f30b3fcf06ea2cdd84f3060b4ed94ba051e545
yash6012 <yash7singh3@gmail.com>
2025-06-26 20:54:01 +0530

    Level Order

--- Generic_Tree/Traversal.cpp
+++ Generic_Tree/Traversal.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class TreeNode{
@@ -60,6 +61,33 @@ class TreeNode{
         // R
         postorder(root -> right);
         cout << root -> val << endl;
+    }
+    void levelorder(TreeNode* root) {
+        if (root == NULL) {
+            return;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        while (!q.empty()) {
+            TreeNode* front = q.front();
+            q.pop();
+            if (front == NULL) {
+                cout << endl;
+                if (!q.empty()) {
+                    q.push(NULL);
+                }
+            }
+            else {
+                cout << front -> val;
+                if (front -> left != NULL) {
+                    q.push(front -> left);
+                }
+                if (front -> right != NULL) {
+                    q.push(front -> right);
+                }
+            }
+        }
     }
 };
 
