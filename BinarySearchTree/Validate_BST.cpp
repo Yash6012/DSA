16c7716cf30639e3a2cd4437e584b8ce232093a8
yash6012 <yash7singh3@gmail.com>
2025-07-02 22:47:25 +0530

    Other Functions

--- BinarySearchTree/Validate_BST.cpp
+++ BinarySearchTree/Validate_BST.cpp
@@ -1,6 +1,40 @@
-class Info {
-    public:
-        long long minval;
-        long long maxval;
-        bool isB;
+#include <iostream>
+using namespace std;
+
+class Info
+{
+public:
+    long long minval;
+    long long maxval;
+    bool isB;
 };
+Info solve(TreeNode *root)
+{
+    // Base Case
+    if (root == NULL)
+    {
+        Info temp;
+        temp.minval = LLONG_MAX;
+        temp.maxval = LLONG_MIN;
+        temp.isB = true;
+        return temp;
+    }
+    // leaf Node
+    if (root->left == NULL && root->right == NULL)
+    {
+        Info temp;
+        temp.minval = root->val;
+        temp.maxval = root->val;
+        temp.isB = true;
+        return temp;
+    }
+    // LRN
+    Info leftA = solve(root->left);
+    Info rightA = solve(root->right);
+    // Node
+    Info currA;
+    currA.minval = min(leftA.minval, min(rightA.minval, (long long)root->val));
+    currA.maxval = max(leftA.maxval, max(rightA.maxval, (long long)root->val));
+    currA.isB = (root->val > leftA.maxval && root->val < rightA.minval && leftA.isB && rightA.isB);
+    return currA;
+}
